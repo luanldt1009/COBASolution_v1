55ee143b65eedd14304d8adce310e1314a8e8132
le luan <ledangthanhluan@outlook.com>
2021-03-08 16:10:20 +0700

    init Utilities

---
+++ COBAShop.Utilities/Exceptions/COBAShopException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COBAShop.Utilities.Exceptions
+{
+    public class COBAShopException : Exception
+    {
+        public COBAShopException()
+        {
+        }
+
+        public COBAShopException(string message) : base(message)
+        {
+        }
+
+        public COBAShopException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
